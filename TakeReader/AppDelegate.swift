23b814d1c09bde7d14f50f18e916d1e5ab205b1f
vwolf <info@devwolf.de>
2020-03-12 09:47:18 +0100

    Initial Commit

---
+++ TakeReader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TakeReader
+//
+//  Created by Volker Wolf on 12/03/20.
+//  Copyright © 2020 devWolf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TakeReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TakeReader
+//
+//  Created by Volker Wolf on 12/03/20.
+//  Copyright © 2020 devWolf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
